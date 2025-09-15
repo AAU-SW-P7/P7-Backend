158090e3df534acb6c2aa9892f58c8338568b8d3
Allan Vester <vesterallan246@gmail.com>
2025-09-15 22:42:46 +0000

    Moved dir

--- p7/urls.py
+++ p7/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from backend.api.views import hello
+from api.views import hello
 
 urlpatterns = [
     path("admin/", admin.site.urls),
