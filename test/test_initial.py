b27ce61a9100c052533ac19159f86945397b6867
Peterrahr <Prni22@student.aau.dk>
2025-10-03 12:36:00 +0200

    add initial test

--- test/test_initial.py
+++ test/test_initial.py
@@ -6,3 +6,10 @@ def test_func():
     assert func(3) == 6
     assert func(-1) == -2
     assert func(0) == 0
+
+
+def address():
+    return "123 Main St, Anytown, USA"
+
+def test_address():
+    assert address() == "123 Main St, Anytown, USA"    
