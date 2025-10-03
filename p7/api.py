27f866df962e4dda68ea1703328f90187a87fac8
Peterrahr <Prni22@student.aau.dk>
2025-10-03 14:08:03 +0200

    fix linting

--- p7/api.py
+++ p7/api.py
@@ -1,11 +1,12 @@
+"""API routing for the P7 backend."""
 from ninja import NinjaAPI
-from .hello.api import router as hello_router
 from .getDropboxFiles.api import router as dropbox_router
+from .getOnedriveFiles.api import router as onedrive_router
 
 
 api = NinjaAPI()
 
 
-api.add_router("/hello/", hello_router)
 api.add_router("/getDropboxFiles/", dropbox_router)
+api.add_router("/getOnedriveFiles/", onedrive_router)
 # You can add other routers similarly
