26a1a3bf82bd43e585397fcf77ffd667db0e5709
Allan Vester <vesterallan246@gmail.com>
2025-10-10 12:55:21 +0200

    New sql.sql

--- sql.sql
+++ sql.sql
@@ -1,9 +1,17 @@
+-- Users
 CREATE TABLE "users" (
-  "id" SERIAL PRIMARY KEY
+  "id" BIGSERIAL PRIMARY KEY
 );
 
+-- Term (termName is a natural key; keep as UNIQUE so FKs can reference it)
+CREATE TABLE "term" (
+  "termName" TEXT NOT NULL,
+  CONSTRAINT "uq_term_name" UNIQUE ("termName")
+);
+
+-- Service
 CREATE TABLE "service" (
-  "id" SERIAL PRIMARY KEY,
+  "id" BIGSERIAL PRIMARY KEY,
   "userId" BIGINT NOT NULL,
   "oauthType" TEXT NOT NULL,
   "oauthToken" TEXT NOT NULL,
@@ -13,11 +21,14 @@ CREATE TABLE "service" (
   "name" TEXT NOT NULL,
   "accountId" TEXT NOT NULL,
   "email" TEXT NOT NULL,
-  "scopeName" TEXT NOT NULL
+  "scopeName" TEXT NOT NULL,
+  CONSTRAINT "fk_service_user"
+    FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE
 );
 
+-- File
 CREATE TABLE "file" (
-  "id" SERIAL PRIMARY KEY,
+  "id" BIGSERIAL PRIMARY KEY,
   "serviceId" BIGINT NOT NULL,
   "serviceFileId" TEXT NOT NULL,
   "name" TEXT NOT NULL,
@@ -30,68 +41,32 @@ CREATE TABLE "file" (
   "modifiedAt" TIMESTAMPTZ NOT NULL,
   "lastIndexed" TIMESTAMPTZ,
   "snippet" TEXT,
-  "content" TEXT
+  "content" TEXT,
+  CONSTRAINT "fk_file_service"
+    FOREIGN KEY ("serviceId") REFERENCES "service"("id") ON DELETE CASCADE,
+  CONSTRAINT "uq_service_file_id" UNIQUE ("serviceId", "serviceFileId")
 );
 
-CREATE TABLE "term" (
-  "termName" TEXT NOT NULL
-);
-
+-- Inverted Index (per-user term stats)
 CREATE TABLE "invertedindex" (
   "userId" BIGINT NOT NULL,
   "termName" TEXT NOT NULL,
-  "documentFrequency" BIGINT NOT NULL
+  "documentFrequency" BIGINT NOT NULL,
+  CONSTRAINT "fk_inv_user"
+    FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE,
+  CONSTRAINT "fk_term_name"
+    FOREIGN KEY ("termName") REFERENCES "term"("termName") ON DELETE CASCADE,
+  CONSTRAINT "uq_inv_user_term_name" UNIQUE ("userId", "termName")
 );
 
+-- Posting list (per-file term stats)
 CREATE TABLE "posting" (
   "termName" TEXT NOT NULL,
   "fileId" BIGINT NOT NULL,
-  "termFrequency" BIGINT NOT NULL
+  "termFrequency" BIGINT NOT NULL,
+  CONSTRAINT "fk_posting_term"
+    FOREIGN KEY ("termName") REFERENCES "term"("termName") ON DELETE CASCADE,
+  CONSTRAINT "fk_posting_file"
+    FOREIGN KEY ("fileId") REFERENCES "file"("id") ON DELETE CASCADE,
+  CONSTRAINT "uq_term_name_file_id" UNIQUE ("termName", "fileId")
 );
-
-CREATE UNIQUE INDEX "uq_service_file_id" ON "file" ("serviceId", "serviceFileId");
-
-CREATE UNIQUE INDEX "uq_term_name" ON "term" ("termName");
-
-CREATE UNIQUE INDEX "uq_inv_user_term_name" ON "invertedindex" ("userId", "termName");
-
-CREATE UNIQUE INDEX "uq_term_name_file_id" ON "posting" ("termName", "fileId");
-
-ALTER TABLE "service" ADD CONSTRAINT "fk_service_user" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE CASCADE;
-
-ALTER TABLE "file" ADD CONSTRAINT "fk_file_service" FOREIGN KEY ("serviceId") REFERENCES "service" ("id") ON DELETE CASCADE;
-
-CREATE TABLE "users_invertedindex" (
-  "users_id" SERIAL,
-  "invertedindex_userId" BIGINT,
-  PRIMARY KEY ("users_id", "invertedindex_userId")
-);
-
-ALTER TABLE "users_invertedindex" ADD FOREIGN KEY ("users_id") REFERENCES "users" ("id");
-
-ALTER TABLE "users_invertedindex" ADD FOREIGN KEY ("invertedindex_userId") REFERENCES "invertedindex" ("userId");
-
-
-CREATE TABLE "term_posting" (
-  "term_termName" TEXT,
-  "posting_termName" TEXT,
-  PRIMARY KEY ("term_termName", "posting_termName")
-);
-
-ALTER TABLE "term_posting" ADD FOREIGN KEY ("term_termName") REFERENCES "term" ("termName");
-
-ALTER TABLE "term_posting" ADD FOREIGN KEY ("posting_termName") REFERENCES "posting" ("termName");
-
-
-ALTER TABLE "posting" ADD CONSTRAINT "fk_posting_file" FOREIGN KEY ("fileId") REFERENCES "file" ("id") ON DELETE CASCADE;
-
-CREATE TABLE "invertedindex_term" (
-  "invertedindex_termName" TEXT,
-  "term_termName" TEXT,
-  PRIMARY KEY ("invertedindex_termName", "term_termName")
-);
-
-ALTER TABLE "invertedindex_term" ADD FOREIGN KEY ("invertedindex_termName") REFERENCES "invertedindex" ("termName");
-
-ALTER TABLE "invertedindex_term" ADD FOREIGN KEY ("term_termName") REFERENCES "term" ("termName");
-
