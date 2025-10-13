8672e5ab2a403f4dc21fd1ff9ab11f1fd99d8fc8
Lazcse <lassefonager@hotmail.com>
2025-10-13 13:55:51 +0200

    Fixed most PyLint problems in file.py

--- repository/file.py
+++ repository/file.py
@@ -1,32 +1,34 @@
+"""Repository functions for handling File model operations."""
 from repository.models import File
 
 def save_file(
-    sericeId,
-    serviceFileId,
+    service_id,
+    service_file_id,
     name,
     extension,
     downloadable,
     path,
     link,
     size,
-    createdAt,
-    modifiedAt,
-    lastIndexed,
+    created_at,
+    modified_at,
+    last_indexed,
     snippet,
     content,
 ):
+    """Saves or updates a file record in the database."""
     File.objects.update_or_create(
-        serviceId=sericeId,
-        serviceFileId=serviceFileId,
+        serviceId=service_id,
+        serviceFileId=service_file_id,
         name=name,
         extension=extension,
         downloadable=downloadable,
         path=path,
         link=link,
         size=size,
-        createdAt=createdAt,
-        modifiedAt=modifiedAt,
-        lastIndexed=lastIndexed,
+        createdAt=created_at,
+        modifiedAt=modified_at,
+        lastIndexed=last_indexed,
         snippet=snippet,
         content=content,
     )
