0437bfda315bf74a1bfdbca445dc3507685f74b0
Allan Vester <vesterallan246@gmail.com>
2025-10-16 14:01:15 +0200

    Fixed pylint

--- test/helpers/save_file.py
+++ test/helpers/save_file.py
@@ -1,8 +1,8 @@
 """Helper functions for testing save files endpoint."""
 import os
-from p7.get_google_drive_files.api import build_google_drive_path
 import pytest_check as check
 
+from p7.get_google_drive_files.api import build_google_drive_path
 from repository.models import Service, User, File
 
 def assert_save_file_success(client, user_id, service_name):
@@ -22,8 +22,6 @@ def assert_save_file_success(client, user_id, service_name):
     # Assuming 3 users are already created for service creation
     check.equal(initial_user_count == 3, True)
     check.equal(initial_service_count == 1, True)
-    
-    print(f"Initial service count for user {user_id} and service {service_name}: {initial_service_count}")
 
     response = client.get(
         f"/?user_id={user_id}",
@@ -31,10 +29,10 @@ def assert_save_file_success(client, user_id, service_name):
     )
 
     data = response.json()
-    
+
     check.equal(response.status_code, 200)
     check.is_instance(data, list)
-    
+
     for file in data:
         check.is_instance(file, dict)
         if service_name == 'dropbox':
@@ -46,9 +44,9 @@ def assert_save_file_success(client, user_id, service_name):
             # check.is_instance(file.get('server_modified'), str)
             # check.is_instance(file.get('size'), int)
             # check.is_instance(file.get('is_downloadable'), bool)
-            
+
             # check.equal(file.get('.etag'), 'file')
-            
+
             extension = os.path.splitext(file["name"])[1]
             path = file["path_display"]
             link = "https://www.dropbox.com/preview" + path
@@ -70,13 +68,14 @@ def assert_save_file_success(client, user_id, service_name):
                 content=None,
             ).count()
             check.equal(get_file, 1)
+
         elif service_name == 'google':
             file_by_id = {file["id"]: file for file in data}
-            
+
             extension = os.path.splitext(file.get("name", ""))[1]
             downloadable = file.get("capabilities", {}).get("canDownload")
             path = build_google_drive_path(file, file_by_id)
-            
+
             get_file = File.objects.filter(
                 serviceId__userId=user_id,
                 serviceId__name=service_name,
@@ -94,6 +93,7 @@ def assert_save_file_success(client, user_id, service_name):
                 content=None,
             ).count()
             check.equal(get_file, 1)
+
         elif service_name == 'microsoft-entra-id':
             extension = os.path.splitext(file["name"])[1]
             path = (
@@ -103,7 +103,7 @@ def assert_save_file_success(client, user_id, service_name):
                 + "/"
                 + file["name"]
             )
-            
+
             get_file = File.objects.filter(
                 serviceId__userId=user_id,
                 serviceId__name=service_name,
--- test/test_save_file.py
+++ test/test_save_file.py
@@ -188,84 +188,87 @@ def test_create_service_missing_payload(service_client):
     params:
         service_client: Fixture for creating a test client for the create_service endpoint.
     """
-    for user_number in range(1, 3+1):  # 3 users
-    
-        assert_create_service_missing_payload(service_client)
+
+    assert_create_service_missing_payload(service_client)
 
 def test_save_dropbox_file_success(save_dropbox_file_client_fixture):
     """Test saving a Dropbox file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
 
         assert_save_file_success(save_dropbox_file_client_fixture, user_number, 'dropbox')
 
 def test_save_dropbox_file_invalid_auth(save_dropbox_file_client_fixture):
     """Test saving a Dropbox file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
-        
+
         assert_save_file_invalid_auth(save_dropbox_file_client_fixture, user_number)
-    
+
 def test_save_dropbox_file_missing_header(save_dropbox_file_client_fixture):
     """Test saving a Dropbox file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
 
         assert_save_file_missing_header(save_dropbox_file_client_fixture, user_number)
 
 def test_save_dropbox_file_missing_user_id(save_dropbox_file_client_fixture):
     """Test saving a Dropbox file."""
-    
+
     assert_save_file_missing_user_id(save_dropbox_file_client_fixture)
 
 def test_save_google_drive_file_success(save_google_drive_file_client_fixture):
     """Test saving a Google Drive file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
 
         assert_save_file_success(save_google_drive_file_client_fixture, user_number, 'google')
 
 def test_save_google_drive_file_invalid_auth(save_google_drive_file_client_fixture):
     """Test saving a Google Drive file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
-        
+
         assert_save_file_invalid_auth(save_google_drive_file_client_fixture, user_number)
 
 def test_save_google_drive_file_missing_header(save_google_drive_file_client_fixture):
     """Test saving a Google Drive file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
 
         assert_save_file_missing_header(save_google_drive_file_client_fixture, user_number)
 
 def test_save_google_drive_file_missing_user_id(save_google_drive_file_client_fixture):
     """Test saving a Google Drive file."""
-    
+
     assert_save_file_missing_user_id(save_google_drive_file_client_fixture)
 
 def test_save_onedrive_file_success(save_onedrive_file_client_fixture):
     """Test saving a OneDrive file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
 
-        assert_save_file_success(save_onedrive_file_client_fixture, user_number, 'microsoft-entra-id')
-    
+        assert_save_file_success(
+            save_onedrive_file_client_fixture,
+            user_number,
+            'microsoft-entra-id',
+        )
+
 def test_save_onedrive_file_invalid_auth(save_onedrive_file_client_fixture):
     """Test saving a OneDrive file."""
 
     for user_number in range(1, 3+1):  # 3 users
-        
+
         assert_save_file_invalid_auth(save_onedrive_file_client_fixture, user_number)
 
 def test_save_onedrive_file_missing_header(save_onedrive_file_client_fixture):
     """Test saving a OneDrive file."""
-    
+
     for user_number in range(1, 3+1):  # 3 users
 
         assert_save_file_missing_header(save_onedrive_file_client_fixture, user_number)
-    
+
 def test_save_onedrive_file_missing_user_id(save_onedrive_file_client_fixture):
     """Test saving a OneDrive file."""
-    
+
     assert_save_file_missing_user_id(save_onedrive_file_client_fixture)
