8c2684188024a0b9e05f09406eea25645a969296
Simon Thordal <35608034+SimonSkywalker@users.noreply.github.com>
2025-11-05 11:00:56 +0100

    updated cluster settings in test folder

--- test/test_settings.py
+++ test/test_settings.py
@@ -1,5 +1,9 @@
 """Test settings for the Django application."""
+from math import ceil, floor
+import multiprocessing
 import os
+
+from p7 import settings as p7_settings
 
 SECRET_KEY = "bogus"
 
@@ -18,16 +22,25 @@ DATABASES = {
         "PORT": os.getenv("DATABASE_PORT"),
     }
 }
-
+Q_CLUSTER = p7_settings.Q_CLUSTER.copy()
 # DJANGO_Q database config. Docs: https://django-q2.readthedocs.io/en/master/configure.html
-Q_CLUSTER = {
-    'name': 'default',
-    'retry': 3600,
-    'timeout': 600,
-    'recycle': 250,
-    'save_limit': 10,
-    'queue_limit': 100,
-    'cpu_affinity': 1,
-    'label': 'Django Q2',
-    'orm': 'default',
-}
+# Q_CLUSTER = {
+#     'name': 'default',
+#     'workers': multiprocessing.cpu_count(),
+#     'retry': 3600,
+#     'timeout': 600,
+#     'recycle': 250,
+#     'save_limit': 10,
+#     'queue_limit': 100,
+#     'cpu_affinity': 1,
+#     'label': 'Django Q2',
+#     'orm': 'default',
+#     'ALT_CLUSTERS':{
+#         'low': {
+#             'workers': ceil(multiprocessing.cpu_count()*0.25),
+#         },
+#         'high': {
+#             'workers': floor(multiprocessing.cpu_count()*0.75),
+#         },
+#    }
+# }
