05b48e63e9cda953016f570deb52f93b6ae8b47b
Simon Thordal <35608034+SimonSkywalker@users.noreply.github.com>
2025-11-05 09:35:16 +0100

    removed admin panels

--- p7/settings.py
+++ p7/settings.py
@@ -31,12 +31,12 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',  # Creates default tables which we do not want
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.admin',
+    #'django.contrib.auth',  # Creates default tables which we do not want
+    #'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.messages',
+    #'django.contrib.staticfiles',
     "corsheaders",
     # "repository.apps.RepositoryConfig",
     "repository",
@@ -45,11 +45,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
 ]
@@ -103,8 +103,8 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+                #'django.contrib.auth.context_processors.auth',
+                #'django.contrib.messages.context_processors.messages',
             ],
         },
     },
--- p7/urls.py
+++ p7/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from django.contrib import admin  # --- IGNORE ---
+#from django.contrib import admin  # --- IGNORE ---
 from .api import api
 
 urlpatterns = [
     path("api/", api.urls),
-    path("admin/", admin.site.urls),  # --- IGNORE ---
+    #path("admin/", admin.site.urls),  # --- IGNORE ---
 ]
