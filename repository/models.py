add90669a4256fe23b039ac4321d57af8a27a7be
Skjodt <113089138+Skjodt@users.noreply.github.com>
2025-11-12 14:04:28 +0100

    Removed duplicate fields

--- repository/models.py
+++ repository/models.py
@@ -81,10 +81,6 @@ class File(models.Model):
     modifiedAt = pgcrypto.EncryptedDateTimeField()
     indexedAt = pgcrypto.EncryptedDateTimeField(null=True, blank=True)
     snippet = pgcrypto.EncryptedTextField(null=True, blank=True)
-    createdAt = models.DateTimeField()
-    modifiedAt = models.DateTimeField()
-    indexedAt = models.DateTimeField(null=True, blank=True)
-    snippet = models.TextField(null=True, blank=True)
     ts = SearchVectorField(null=True)
 
     class Meta:
