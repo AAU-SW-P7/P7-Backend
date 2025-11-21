39a1a6c8ccc89855a986b8a6196a436438df1d35
Allan Vester <vesterallan246@gmail.com>
2025-11-21 13:29:16 +0100

    Updated prod with Gunicorn

--- p7/settings.py
+++ p7/settings.py
@@ -130,7 +130,6 @@ DATABASES = {
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
         "PORT": os.getenv("DATABASE_PORT"),
-        "CONN_MAX_AGE":60
     }
 }
 # DJANGO_Q database config. Docs: https://django-q2.readthedocs.io/en/master/configure.html
