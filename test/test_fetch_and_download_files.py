bb770343239d49935398c6f2abd2f7e2758fb417
Lazcse <lassefonager@hotmail.com>
2025-11-25 11:18:52 +0100

    Fixed missing imports

--- test/test_fetch_and_download_files.py
+++ test/test_fetch_and_download_files.py
@@ -31,7 +31,13 @@ from helpers.fetch_service import (
     assert_fetch_onedrive_files_missing_header,
     assert_fetch_onedrive_files_missing_userid,
 )
-from helpers.sync_files import create_service
+from helpers.download_file import (
+    assert_download_file_success,
+    assert_download_file_invalid_auth,
+    assert_download_file_missing_header,
+    assert_download_file_missing_user_id,
+)
+from helpers.general_helper_functions import create_service, create_x_users
 
 from p7.get_dropbox_files.api import fetch_dropbox_files_router
 from p7.get_google_drive_files.api import fetch_google_drive_files_router
