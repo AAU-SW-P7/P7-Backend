500e8f04848a2cf76f70ac6158979688f3498d34
Lazcse <lassefonager@hotmail.com>
2025-12-03 13:26:07 +0100

    Added a test that acts more like a user

---
+++ test/locust_test/user_like_test.py
@@ -0,0 +1,22 @@
+"""Locust load test for the search endpoint."""
+import random
+import time
+from locust import HttpUser, task, between
+
+class SearchLoadTestUser(HttpUser):
+    """A Locust user class to perform load testing on the search endpoint."""
+    wait_time = between(20, 45)
+
+    @task
+    def search(self):
+        """Task to perform search requests."""
+        query = ["test","test account","test case","test plan","test account plan"]
+        user_id = 1  # User id that all the load test requests will use
+        random_nr = random.randint(0, 4)
+        for nr in range(random_nr):
+            self.client.get(
+                f"/api/search/?user_id={user_id}&search_string={query[nr]}",
+                headers={"x-internal-auth": "p7"}
+                )
+            wait_duration = random.uniform(10, 20)
+            time.sleep(wait_duration)
