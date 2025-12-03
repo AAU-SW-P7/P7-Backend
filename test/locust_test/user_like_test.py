9c5b8ad57f555065304cb41ad2443a1d74caa0b4
Lazcse <lassefonager@hotmail.com>
2025-12-03 14:28:52 +0100

    Further fixes

--- test/locust_test/user_like_test.py
+++ test/locust_test/user_like_test.py
@@ -1,6 +1,7 @@
 """Locust load test for the search endpoint."""
 import random
 import time
+from urllib.parse import quote
 from locust import HttpUser, task, between
 
 class SearchLoadTestUser(HttpUser):
@@ -10,13 +11,19 @@ class SearchLoadTestUser(HttpUser):
     @task
     def search(self):
         """Task to perform search requests."""
-        query = ["test","test account","test case","test plan","test account plan"]
+        queries = ["test", "test account", "test case", "test plan", "test account plan"]
         user_id = 1  # User id that all the load test requests will use
-        random_nr = random.randint(1, 5)
-        for nr in range(0, random_nr):
+
+        # Fix: Ensure we don't exceed array bounds
+        random_nr = random.randint(1, len(queries))
+
+        for nr in range(random_nr):
+            # URL encode the search string to handle spaces and special characters
+            search_string = quote(queries[nr])
             self.client.get(
-                f"/api/search/?user_id={user_id}&search_string={query[nr]}",
+                f"/api/search/?user_id={user_id}&search_string={search_string}",
                 headers={"x-internal-auth": "p7"}
-                )
+            )
+            # Simulate user reading/processing time between searches
             wait_duration = random.uniform(10, 20)
             time.sleep(wait_duration)
