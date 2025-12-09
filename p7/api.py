a2fe3ea5be44b0aa77160e9c46ef7a0e9cdd0f0c
Skjodt <113089138+Skjodt@users.noreply.github.com>
2025-12-09 15:06:19 +0100

    Added Endpoints needed for performance test

--- p7/api.py
+++ p7/api.py
@@ -5,10 +5,14 @@ from ninja import NinjaAPI
 from p7.get_dropbox_files.api import fetch_dropbox_files_router
 from p7.get_google_drive_files.api import fetch_google_drive_files_router
 from p7.get_onedrive_files.api import fetch_onedrive_files_router
+from p7.get_local_files.api import fetch_local_files_router
+from p7.test_prepare_download_google_drive_files.api import fetch_test_prepare_download_google_drive_files_router
 
 from p7.download_dropbox_files.api import download_dropbox_files_router
 from p7.download_onedrive_files.api import download_onedrive_files_router
 from p7.download_google_drive_files.api import download_google_drive_files_router
+from p7.download_local_files.api import download_local_files_router
+from p7.test_download_files.api import test_download_files_router
 
 from p7.sync_files.api import sync_files_router
 from p7.create_user.api import create_user_router
@@ -24,10 +28,14 @@ api = NinjaAPI()
 api.add_router("/fetch_dropbox_files/", fetch_dropbox_files_router)
 api.add_router("/fetch_google_drive_files/", fetch_google_drive_files_router)
 api.add_router("/fetch_onedrive_files/", fetch_onedrive_files_router)
+api.add_router("/fetch_local_files/", fetch_local_files_router)
+api.add_router("/fetch_test_prepare_download_google_drive_files/", fetch_test_prepare_download_google_drive_files_router)
 
 api.add_router("/download_google_drive_files/", download_google_drive_files_router)
 api.add_router("/download_dropbox_files/", download_dropbox_files_router)
 api.add_router("/download_onedrive_files/", download_onedrive_files_router)
+api.add_router("/download_local_files/", download_local_files_router)
+api.add_router("/test_download_files/", test_download_files_router)
 
 api.add_router("/sync_files/", sync_files_router)
 
---
+++ p7/download_local_files/api.py
@@ -0,0 +1,50 @@
+from ninja import Router, Header
+from django.http import JsonResponse
+from django_q.tasks import async_task
+from p7.helpers import validate_internal_auth
+from repository.service import get_service
+from repository.user import get_user
+from p7.get_local_files.helper import fetch_recursive_local_files
+from p7.download_local_files.helper import download_recursive_local_files
+
+download_local_files_router = Router()
+
+
+@download_local_files_router.get("/")
+def download_local_files(
+    request,
+    user_id: str,
+    x_internal_auth: str = Header(..., alias="x-internal-auth"),
+):
+    auth_resp = validate_internal_auth(x_internal_auth)
+    if auth_resp:
+        return auth_resp
+
+    user = get_user(user_id)
+    if isinstance(user, JsonResponse):
+        return user
+
+    task_id = async_task(
+        process_download_local_files,
+        user_id,
+        cluster="high",
+        group=f"Local-{user_id}"
+    )
+
+    return JsonResponse({"task_id": task_id, "status": "processing"}, status=202)
+
+
+def process_download_local_files(user_id):
+    """Read file content + update tsvector."""
+    try:
+        service = get_service(user_id, "google")
+        processed, errors = download_recursive_local_files(service)
+
+        return {
+            "files": processed,
+            "errors": errors
+        }
+
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+    
