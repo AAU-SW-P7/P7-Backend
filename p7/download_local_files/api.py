aab965c3ae3ed9b3bbc524a26bceeb226fd740a7
Skjodt <113089138+Skjodt@users.noreply.github.com>
2025-12-09 16:56:29 +0100

    fixed pylint

--- p7/download_local_files/api.py
+++ p7/download_local_files/api.py
@@ -4,8 +4,9 @@ from ninja import Router, Header
 from django.http import JsonResponse
 from django_q.tasks import async_task
 from p7.helpers import validate_internal_auth
-from repository import service, user
 from p7.download_local_files.helper import download_recursive_local_files
+from repository.service import get_service
+from repository.user import get_user
 
 download_local_files_router = Router()
 
@@ -21,9 +22,9 @@ def download_local_files(
     if auth_resp:
         return auth_resp
 
-    existing_user = user.get_user(user_id)
-    if isinstance(existing_user, JsonResponse):
-        return existing_user
+    user = get_user(user_id)
+    if isinstance(user, JsonResponse):
+        return user
 
     task_id = async_task(
         process_download_local_files,
@@ -38,8 +39,8 @@ def download_local_files(
 def process_download_local_files(user_id):
     """Read file content + update tsvector."""
     try:
-        service_id = service.get_service(user_id, "google")
-        processed, errors = download_recursive_local_files(service_id)
+        service = get_service(user_id, "google")
+        processed, errors = download_recursive_local_files(service)
 
         return {
             "files": processed,
