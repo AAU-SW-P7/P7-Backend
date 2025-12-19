8a6b457ad617523d5600b1b11b47b145749ee6de
Laczse <lassefonager@hotmail.com>
2025-12-19 11:18:03 +0100

    Added Locust load test (#94)

---
+++ test/locust_test/search_load_test.py
@@ -0,0 +1,16 @@
+"""Locust load test for the search endpoint."""
+from locust import HttpUser, task, between
+
+class SearchLoadTestUser(HttpUser):
+    """A Locust user class to perform load testing on the search endpoint."""
+    wait_time = between(5, 10)
+
+    @task
+    def search(self):
+        """Task to perform search requests."""
+        query = "test"
+        user_id = 1  # User id that all the load test requests will use
+        self.client.get(
+            f"/api/search/?user_id={user_id}&search_string={query}",
+            headers={"x-internal-auth": "p7"}
+            )
